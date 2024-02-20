612d6ea82a2b95aa317bc540a7ef9fc494b8d88e
Sheldon Reimers <swrei92@gmail..com>
2024-02-20 17:28:59 +0200

    repo setup

--- inventory_manager/inventory.py
+++ inventory_manager/inventory.py
@@ -10,11 +10,7 @@ from datetime import timedelta
 from tqdm.notebook import tqdm, trange
 from datetime import datetime as dt
 
-<<<<<<< HEAD
 sys.path.append('config')
-=======
-sys.path.append('/config')
->>>>>>> f8db2bd (repo setup)
 from lib import GoogleSheets
 from lib import ServiceM8
 
