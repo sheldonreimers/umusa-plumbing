c9f2f944707b987ddd88a92f6bf9babdca32bd72
Sheldon Reimers <swrei92@gmail..com>
2024-02-20 15:42:32 +0200

    repo setup

--- inventory_manager/inventory.py
+++ inventory_manager/inventory.py
@@ -3,24 +3,12 @@ import os, sys
 home_dir = 'umusa-plumbing'
 
 # Working Libraries
-import jira,shlex,time,requests,subprocess,json,io,shutil,csv,requests,json,zipfile,datetime,pytz, atexit
+import time,requests,json,pytz
 import pandas as pd
-import numpy as np
 
 from datetime import timedelta
-from datetime import time
 from tqdm.notebook import tqdm, trange
-from ast import literal_eval
-from pretty_html_table import build_table
 from datetime import datetime as dt
-from pretty_html_table import build_table
-
-# Google Libraries
-from apiclient.http import MediaIoBaseDownload
-from googleapiclient.http import MediaFileUpload
-from googleapiclient.discovery import build
-from oauth2client.service_account import ServiceAccountCredentials
-from google.oauth2 import service_account
 
 sys.path.append(home_dir+'/config')
 from lib import GoogleSheets
