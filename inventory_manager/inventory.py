6ab2dde90d416fdd4a7a996c956a2ef7aa9aa93c
Sheldon Reimers <swrei92@gmail..com>
2024-02-20 17:35:21 +0200

    repo setup

--- inventory_manager/inventory.py
+++ inventory_manager/inventory.py
@@ -1,6 +1,6 @@
 # System Library Import & directories
-import os, sys
-home_dir = 'umusa-plumbing'
+import os
+import sys
 
 # Working Libraries
 import time,requests,json,pytz
@@ -10,8 +10,15 @@ from datetime import timedelta
 from tqdm.notebook import tqdm, trange
 from datetime import datetime as dt
 
+# Add the directory containing your custom libraries to the path
 sys.path.append('config')
 from lib import GoogleSheets
 from lib import ServiceM8
 
-print(os.environ.get('UMUSA_SECRET'))
+# Access the secrets from environment variables
+umusa_secret = os.environ.get('UMUSA_SECRET')
+servicem8_secret = os.environ.get('SERVICEM8_SECRET')
+
+# Print the secrets to verify
+print("UMUSA_SECRET:", umusa_secret)
+print("SERVICEM8_SECRET:", servicem8_secret)
